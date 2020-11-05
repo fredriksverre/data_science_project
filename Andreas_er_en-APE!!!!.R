68a1bf12741349da1e7edd7fd28d362c8a35dd21
Skundberg Andreas Reiten (ask097) <ask097@rstudio.uit.no>
2020-11-05 13:27:57 +0100

    api

--- Andreas_er_en-APE!!!!.R
+++ Andreas_er_en-APE!!!!.R
@@ -3,3 +3,17 @@
 #oppdatert....
 
 #link: https://ourworldindata.org/
+
+
+# https://metals-api.com/api/latest
+
+access_key = okrh96bifqusv5w65rpj6w3cs223gf570bs57akec4j4r03aucdze0yjlqkl
+
+
+
+
+https://metals-api.com/api/YYYY-MM-DD
+
+access_key = YOUR_ACCESS_KEY
+& base = USD
+& symbols = XAU,XAG
