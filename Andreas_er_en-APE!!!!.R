6cff919d5e8e3bb801d830e0faa703f7bdb8ea84
Nilssen Fredrik Sverre (fni021) <fni021@rstudio.uit.no>
2020-11-05 13:49:45 +0100

    api2

--- Andreas_er_en-APE!!!!.R
+++ Andreas_er_en-APE!!!!.R
@@ -12,9 +12,16 @@ access_key = okrh96bifqusv5w65rpj6w3cs223gf570bs57akec4j4r03aucdze0yjlqkl
 
 https://metals-api.com/api/YYYY-MM-DD
 
-access_key = YOUR_ACCESS_KEY
-& base = USD
-& symbols = XAU,XAG
+https://metals-api.com/api/convert
+
+? access_key = YOUR_ACCESS_KEY
+& from = USD
+& to = EUR
+& amount = 25
+
+// append an additional "date" parameter if you want to use
+// historical rates for your conversion
+& date = YYYY-MM-DD
 
 
 Hei
