d856ad26d511ff15c1f56d82f86352c586a44a14
heggelund <alf462@gmail.com>
2020-12-14 15:25:44 +0100

    s

--- Prosjekt_weeklydata.R
+++ Prosjekt_weeklydata.R
@@ -1,22 +1,3 @@
-#Linker Kilder
-
-#Bitcoin
-#https://finance.yahoo.com/quote/BTC-USD/history?period1=1446768000&period2=1604620800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true
-
-#Ethereum
-#https://finance.yahoo.com/quote/ETH-USD/history?period1=1446768000&period2=1604620800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true
-
-#Gull
-#https://finance.yahoo.com/quote/GC%3DF/history?period1=1446768000&period2=1604620800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true
-
-#Vix
-#https://finance.yahoo.com/quote/%5EVIX/history?period1=1446768000&period2=1604620800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true
-
-#Brent olje 
-#https://finance.yahoo.com/quote/BZ%3DF/history?period1=1446768000&period2=1604620800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true
-
-#S&P500
-#https://finance.yahoo.com/quote/%5EGSPC/history?period1=1446768000&period2=1604620800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true
 
 library(tidyverse)
 library(httr)
@@ -24,20 +5,35 @@ library(readr)
 library(quantmod)
 library(lubridate)
 library(Quandl)
-#library(raster)
+library(PerformanceAnalytics)
+library(xts)
 
 # Getting data from API 
 Gold <-Quandl("LBMA/GOLD", api_key="4TmA83fLoY_UpQJVjVJu", start_date="2016-01-04", end_date="2021-01-01")
 
+# Removing N/A values
+Gold1 <- na.omit(Gold1)
+
+# Selecting and renaming columns
 Gold <- Gold %>% select("Date", "USD (PM)") %>% rename(Price = "USD (PM)")
+
+# Converting gold_df into xts format
+toDate <- function(x) as.Date(x, origin = "1899-12-30")
+Gold1<- read.zoo(Gold, header = TRUE, sep = ",", FUN = toDate)
+Gold1 <- as.xts(Gold1)
+
+# Changing name on xts dataframe column
+names(Gold1) <- "Price"
 
 # Getting data from yahoo
 ETH <- getSymbols("ETH-USD", auto.assign=FALSE, from="2016-01-04", src='yahoo')
 BTC <- getSymbols("BTC-USD", auto.assign=FALSE, from="2016-01-04", src='yahoo')
 SP500 <- getSymbols("^GSPC", auto.assign=FALSE, from="2016-01-04", src='yahoo') # Datar starter 04.01.2016
-#BrentOil <- getSymbols("BZ=F", auto.assign=FALSE, from="2016-01-04", src='yahoo') # Data fra 04.12.2018
 VIX <- getSymbols("^VIX", auto.assign=FALSE, from="2016-01-04", src='yahoo') # Data starter fra 04.01.2016. Mangler observasjoner i volum, men vi har ikke behov for dette
-#Gold <- getSymbols("GC=F", auto.assign=FALSE, from="2016-01-04", src='yahoo') # Data starter fra 04.01.2016
+
+# Making rolling(dynamic) correlations
+chart.RollingCorrelation(ETH$`ETH-USD.Close`, BTC$`BTC-USD.Close`, width = 24)
+chart.RollingCorrelation(SP500$GSPC.Close, BTC$`BTC-USD.Close`, width = 24)
 
 # Making data from xts to normal df. Picking columns and renameing.
 ETH <- zoo::fortify.zoo(ETH)
@@ -49,22 +45,18 @@ BTC <- BTC %>% select("Index","BTC-USD.Close") %>% rename(Date ="Index", Price =
 SP500 <- zoo::fortify.zoo(SP500)
 SP500 <- SP500 %>% select("Index","GSPC.Close") %>% rename(Date ="Index", Price = "GSPC.Close")
 
-#Gold <- zoo::fortify.zoo(Gold)
-#Gold <- Gold %>% select("Index","GC=F.Close") %>% mutate(Asset = "Gold") %>% rename(Date ="Index", Price = "GC=F.Close")
-
 VIX <- zoo::fortify.zoo(VIX)
 VIX <- VIX %>% select("Index","VIX.Close") %>% rename(Date ="Index", Price = "VIX.Close")
 
-BrentOil <- zoo::fortify.zoo(BrentOil)
-BrentOil <- BrentOil %>% select("Index","BZ=F.Close") %>% rename(Date ="Index", Price = "BZ=F.Close")
-
-# Lager ny variabel som sorterer ukentlig
+# Making new variable that is sorting weekly
 BTC$week <- floor_date(BTC$Date, "week")
+
 # Grouping then new variable week and taking the mean to get weekly price
 BTC <- BTC %>%
   group_by(week) %>%
   summarize(mean = mean(Price))
-#
+
+# Rename and putting new columns with Asset tag
 BTC <- BTC %>% rename(Price = "mean") %>% mutate(Asset = "Bitcoin")
 
 
@@ -76,8 +68,6 @@ ETH <- ETH %>%
   summarize(mean = mean(Price))
 #
 ETH <- ETH %>% rename(Price = "mean") %>% mutate(Asset = "Ethereum")
-
-
 # 
 SP500$week <- floor_date(SP500$Date, "week")
 #
@@ -87,8 +77,7 @@ SP500 <- SP500 %>%
 #
 SP500 <- SP500 %>% rename(Price = "mean") %>% mutate(Asset = "SP500")
 
-
-# Fjerner N/A verdier
+# Removing N/A values
 Gold <- na.omit(Gold)
 # 
 Gold$week <- floor_date(Gold$Date, "week")
@@ -111,10 +100,13 @@ VIX <- VIX %>% rename(Price = "mean") %>% mutate(Asset = "VIX")
 
 # Making LOG percentage change from day to day
 log_returns <- diff(log(ETH$Price), lag=1)
-# Making df og LOG
+
+# Making df off log_returns
 log_returns <- cbind(log_returns)
-# Putting 0 at the top of LOG, to make it same length as other variables
+
+# Putting 0 at the top of log_returns, to make it same length as other variables
 log_returns <- rbind(0,log_returns)
+
 #Binding LOG togheter with main df
 ETH <- cbind(ETH, log_returns)
 
@@ -155,7 +147,7 @@ log_returns <- rbind(0,log_returns)
 VIX <- cbind(VIX, log_returns)
 
 # Making data long
-df <- bind_rows(ETH,BTC,SP500,Gold,VIX) # For store hull i datasettet til gull.
+df <- bind_rows(ETH,BTC,SP500,Gold,VIX)
 
 #df <- df %>%
  # arrange(Asset,Date) 
@@ -163,7 +155,7 @@ df <- bind_rows(ETH,BTC,SP500,Gold,VIX) # For store hull i datasettet til gull.
 # Making log_returns cumulative
 df <- df %>% group_by(Asset)%>% mutate(cumulative = cumsum(log_returns))
 
-# Plotting! Usikker på om grafen er riktig. Synes nedgangen er for liten i bitcoin og ETH
+# Plotting!
 df %>% 
   ggplot(aes(x=week, y=cumulative, group=Asset)) +
   geom_line(aes(color=Asset))+
@@ -189,11 +181,21 @@ raster::cv(SP500$Price)  # SP500 15.20827
 #SP500
 15.3343 / 83 # 0.1847506
 
+# Roling correlation
+
+chart.RollingCorrelation(Gold$Price, BTC$Price, width = 24)
+
 #Korrelasjon 2016-2020
-cor(BTC$Price, ETH$Price) # BTC/ETH på 0.7109
-cor(BTC$Price, Gold$Price) #BTC/Gold 0.6299536
-cor(BTC$Price, SP500$Price) #BTC/SP500 0.8419065
-cor(Gold$Price,SP500$Price)
+cor.test(BTC$Price, ETH$Price) # BTC/ETH på 0.7109
+cor.test(BTC$Price, Gold$Price) #BTC/Gold 0.6299536
+cor.test(BTC$Price, SP500$Price) #BTC/SP500 0.8419065
+cor.test(Gold$Price,SP500$Price)
+
+#BTCETH <-  TTR::runCor(BTC$Price, ETH$Price)
+
+#BTCETH <- as.data.frame(BTCETH)
+
+options(digits = 20)
 
 # Korrelasjon 2018
 ETH2018 <- ETH %>% filter(week>= "2018-01-01" & week <"2019-01-01")
@@ -223,10 +225,10 @@ BTC2020 <- BTC %>% filter(week>= "2020-01-01")
 Gold2020 <- Gold %>% filter(week>= "2020-01-01")
 SP500_2020 <- SP500 %>% filter(week>= "2020-01-01")
 
-cor(BTC2020$Price,ETH2020$Price) #BTC/ETH
-cor(BTC2020$Price,Gold2020$Price) #BTC/Gold 
-cor(BTC2020$Price,SP500_2020$Price) #BTC/SP500 
-cor(Gold2020$Price,SP500_2020$Price) #Gold /SP500
+cor.test(BTC2020$Price,ETH2020$Price) #BTC/ETH
+cor.test(BTC2020$Price,Gold2020$Price) #BTC/Gold 
+cor.test(BTC2020$Price,SP500_2020$Price) #BTC/SP500 
+cor.test(Gold2020$Price,SP500_2020$Price) #Gold /SP500
 
 # Taking log of df
 log_returns2 <- diff(log(BTC2020$Price), lag=1)
@@ -244,11 +246,10 @@ log_returns2 <- cbind(log_returns2)
 log_returns2 <- rbind(0,log_returns2)
 
 Gold2020 <- cbind(Gold2020, log_returns2)
-<<<<<<< HEAD
+
 
 # Making data long
 df2<-bind_rows(BTC2020,Gold2020)
-=======
 # 
 log_returns2 <- diff(log(SP500_2020$Price), lag=1)
 
@@ -260,7 +261,6 @@ SP500_2020 <- cbind(SP500_2020, log_returns2)
 
 # Making data long
 df2<-bind_rows(BTC2020,Gold2020,SP500_2020)
->>>>>>> 6dd7e0656a1e6b10977891c2421eb33bd2a8eb31
 # Making cumulative count
 df2 <- df2 %>% select(week,Price,Asset,log_returns2) %>% group_by(Asset)%>% mutate(cumulative = cumsum(log_returns2))
 # Plotting. Ser på avkastning til Gull og Bitcoin i 2020
@@ -269,49 +269,16 @@ df2 %>%
   geom_line(aes(color=Asset))+
   ylab(expression("Vekst på kumulativ form")) +
   xlab("Daily") +
-<<<<<<< HEAD
   labs(title = "",
        subtitle = "",
-       caption = "")
-=======
   labs(title = "Logaritmisk avkastning på kumulativ form",
        subtitle = "",
        caption = "")+theme(plot.title = element_text(hjust = 0.5))
->>>>>>> 6dd7e0656a1e6b10977891c2421eb33bd2a8eb31
 
-#
-gp = group_by(Asset)
-dplyr::summarize(gp, cor(a, b))
-
-
-df_regresjon <- cbind(BTC,ETH$Price)
-df_regresjon <- df_regresjon %>% select(Price,`ETH$Price`)
-
-regresjon1 <- lm(Price ~ ETH$Price, data= df_regresjon)
-plot(df_regresjon$Price,df_regresjon$`ETH$Price`, pch = 16, col = "blue") 
-abline(regresjon1)
-
-summary(regresjon1)
-
-
-
-
-# Sliter med å finne Olje data.
 
 # Relativ varians (Risiko), coefficient of variation. Hva sier den CV vi har? 
 # CV = sd/mean*100, den sier noe om hvor store svingninger det er i aksjen men ikke noe om avkastning(potential return).
 # Bruke sharp ratio? Hvordan beregne antatt avkastning?
-
-
-
-#lmTemp = lm(Pressure~Temperature, data = pressure) #Create the linear regression
-#plot(pressure, pch = 10, col = "blue") #Plot the results
-#abline(lmTemp) #Add a regression line
-
-
-summary(BTC)
-summary(ETH)
-
 
 # Making a function to calculate coefficient of variation
 #CV <- function(mean, sd){
@@ -320,14 +287,13 @@ summary(ETH)
 
 # Coefficient of variations
 
-
-
-
 #CV(mean = 200.6097, sd = 227.2656) 
 
 #CV(mean = 1379.591, sd = 209.2408) 
 
 #CV(mean = 2665.762, sd = 405.4164)
+
+
 
 
 
